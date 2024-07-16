9db85e1d18428af359502676738921bf25a5fa6c
sanieeme <shoneesani@gmail.com>
2024-07-16 04:56:07 -0700

    comment for update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,6 +3,15 @@
 
 
 def update_topics(mongo_collection, name, topics):
+    """
+    update topics
+    Args:
+        mongo_collection: collection
+        name: string
+        topics: list of strings
+    Returns:
+         list of trings
+    """
     filter = {"name": name}
     topic = {"$set": {"topics": name}}
     mongo_collection.update_one(filter, topic)
